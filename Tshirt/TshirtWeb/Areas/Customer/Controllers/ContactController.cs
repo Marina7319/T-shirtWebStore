263eec9b6accf0c7119ce3be44c8a2ae1097c4b6
Marina7319 <marina-markova@abv.bg>
2024-04-24 21:00:25 +0300

    AddingTagHelper Commit.

---
+++ Tshirt/TshirtWeb/Areas/Customer/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TshirtWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ContactController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tshirt/TshirtWeb/Areas/Customer/Email/EmailSend.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TshirtWeb.Areas.Customer.Email
+{
+    public class EmailSend : TagHelper
+    {
+        public string Address {  get; set; }
+
+        public string Content { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
+            output.Content.SetContent(Content);
+        }
+
+    }
+}
